ed13401bcf263d8a19264ea82f926d4f854641ef
Евгений Волков <evgenijvolkov@MacBook-Pro-Evgenij.local>
2020-08-19 18:51:09 +0300

    last version

--- book/book.go
+++ book/book.go
@@ -2,23 +2,29 @@ package book
 
 import (
 	"errors"
+	"fmt"
 )
 
 type BookList struct {
-	List []Book
+	List []*Book
 }
 
-func (l *BookList) SearchBook(b Book) (Book, int, error) {
+func (l *BookList) Search(b *Book) (int, error) {
 	for i, v := range l.List {
 		if b.Key == v.Key || b.Title == v.Title {
-			return v, i, errors.New("Книга уже существует")
+			b = v
+			return i, errors.New("Книга уже существует")
 		}
 	}
-	return b, -1, nil
+	return -1, nil
 }
 
-func (l *BookList) AppendBook(b Book) {
+func (l *BookList) Append(b *Book) {
 	l.List = append(l.List, b)
+}
+
+func (l *BookList) Remove(index int) {
+	l.List = append(l.List[:index], l.List[index+1:]...)
 }
 
 type Writer struct {
@@ -30,7 +36,66 @@ type Writer struct {
 type Book struct {
 	Key              string
 	Title            string
-	Author           Writer
+	Author           *Writer
 	YearOfPublishing int
-	IssBy            string
+	IssBy            *User
+}
+
+func (b *Book) Get(u *User) error {
+	if b.IssBy == nil {
+		b.IssBy = u
+		u.Holding = append(u.Holding, b)
+	} else {
+		return errors.New("Книга выдана!")
+	}
+	return nil
+}
+
+func (b *Book) Return(u *User) error {
+	index, err := u.SearchInHolding(b)
+	if err != nil {
+		return err
+	}
+	u.RemoveFromHolding(index)
+	return nil
+}
+
+type Users struct {
+	Array []*User
+}
+
+func (us *Users) Search(u *User) error {
+	for _, v := range us.Array {
+		if v.Login == u.Login && v.password == u.password {
+			u = v
+			fmt.Println(u)
+			fmt.Println(&v)
+			return nil
+		}
+	}
+	return errors.New("Пользователь не найден!")
+}
+
+func (us *Users) Append(u *User) {
+	us.Array = append(us.Array, u)
+}
+
+type User struct {
+	Login    string
+	password string
+	admin    bool
+	Holding  []*Book
+}
+
+func (u *User) SearchInHolding(b *Book) (int, error) {
+	for i, v := range u.Holding {
+		if v == b {
+			return i, nil
+		}
+	}
+	return -1, errors.New("Книга не выдана этому пользователю")
+}
+
+func (u *User) RemoveFromHolding(index int) {
+	u.Holding = append(u.Holding[:index], u.Holding[index+1:]...)
 }
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	// "go_library-rsreu-/book"
+)
 
+func main() {
+	ShowMainMenu()
+}
+
+func ShowMainMenu() {
+	fmt.Println("1) Список книг")
+	fmt.Println("2) Найти книгу")
+	fmt.Println("3) Добавить книгу")
 }
